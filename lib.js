0c75aa362f83a758201a58d6434d936f86878ba7
Marcelo Piva <marcelo@tracersoft.com.br>
2014-12-18 19:10:35 -0200

    lib.js structure

---
+++ lib.js
@@ -0,0 +1,16 @@
+module.exports.spacify = function(str) {
+}
+
+String.prototype.spacify = function() {
+}
+
+module.exports.log = function() {
+}
+
+module.exports.User = {
+  _count: 0,
+  count: function(){
+  },
+  new: function (attributes) {
+  }
+}
