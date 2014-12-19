90eee116d43e8d7743b1b14c64bbbfb28dafeefc
Marcelo Piva <marcelo@tracersoft.com.br>
2014-12-19 10:32:30 -0200

    Tracersoft

--- lib.js
+++ lib.js
@@ -1,10 +1,36 @@
 module.exports.spacify = function(str) {
+  
+  return str.split('').join(' ');
+
 }
 
-String.prototype.spacify = function() {
-}
+String.prototype.spacify = function( )
+{
+  return this.split('').join(' ');
+};
 
-module.exports.log = function() {
+
+module.exports.log = function(  ) {
+
+  var x = arguments[ arguments.length - 1 ];
+
+
+  for (var i = 0; i < arguments.length; i++) {
+    console.log(x.log(arguments[i] ) )
+  };
+
+  // window[args]
+
+
+  // return obj.log( str );
+  // args[ ].log = 1;
+
+
+
+  // console.log( args['log'](str) );
+
+
+  // console.log( args );
 }
 
 module.exports.User = {
@@ -13,4 +39,6 @@ module.exports.User = {
   },
   new: function (attributes) {
   }
+
 }
+
